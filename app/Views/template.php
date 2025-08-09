dc39a206f1437081c3c2693a654681a72cd2dfd0
Your Full Name <jademonsalod8@gmail.com>
2025-08-09 18:40:59 +0800

    "Added Bootstrap template and Home controller"

--- app/Views/template.php
+++ app/Views/template.php
@@ -11,7 +11,7 @@
 <!-- Bootstrap Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">CI4</a>
+        <a class="navbar-brand" href="#">"Added Bootstrap template and Home controller"</a>
     </div>
 </nav>
 
