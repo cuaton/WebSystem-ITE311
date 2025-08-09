2c8dee753a132b551ad396ae249758077445a080
Justine Brian <jusinebrian0909@gmail.com>
2025-08-09 21:27:30 +0800

    Bootstrap template and Home controller

--- app/Views/template.php
+++ app/Views/template.php
@@ -11,7 +11,7 @@
 <!-- Bootstrap Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Added Bootstrap template and Home controller</a>
+        <a class="navbar-brand" href="#">CI ITE311</a>
     </div>
 </nav>
 
